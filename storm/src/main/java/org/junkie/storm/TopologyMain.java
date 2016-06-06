e48c94f8034a9bd33ce1742c73bad39e4ac2e166
junkie0901 <xiaojun9012@126.com>
2016-06-06 15:37:28 +0800

    test1

--- storm/src/main/java/org/junkie/storm/TopologyMain.java
+++ storm/src/main/java/org/junkie/storm/TopologyMain.java
@@ -40,7 +40,7 @@ public class TopologyMain {
 		cluster.shutdown();
 		
 		//remote cluster
-//		conf.setMaxTaskParallelism(3);
+		conf.setMaxTaskParallelism(3);
 //		StormSubmitter.submitTopology("Getting-Started-Topologie", conf, builder.createTopology());
 	}
 
